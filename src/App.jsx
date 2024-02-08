fb5bb8ac0edffba56889da38946b768f50a28b1b
DiegoFMendezV <difemeva@hotmail.com>
2024-02-08 18:33:46 -0500

    tools

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from './components/Navbar/navbar'
 import Inicio from './components/Inicio/Inicio'
+import Tools from './components/Tools/Tools'
 import './App.css'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <Navbar/>
       <Inicio/>
+      <Tools/>
     </>
   )
 }
