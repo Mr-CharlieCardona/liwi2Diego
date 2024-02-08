076811f76eec52d4cca6efc1e714e033e9b95cb7
DiegoFMendezV <difemeva@hotmail.com>
2024-02-08 15:47:57 -0500

    banner_inicio_ajustes

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -1,14 +1,15 @@
 import './Inicio.css'
-import banner_1 from '../../assets/img/banner_1.png'
+import banner from '../../assets/img/banner.jpg'
+import person from '../../assets/img/person.png'
 import especialistas from '../../assets/img/especialistas.png'
 
 function Inicio() {
     return (
         <>
-            <div className='banner'>
-                <img className="img_1" src="https://static.vecteezy.com/system/resources/previews/000/677/302/original/abstract-technology-banner-background.jpg" alt="" />
+            <div className='content'>
+                <img className="banner" src={banner} alt="" />
                 <img className="especialistas" src={especialistas} alt="" />
-                <img className="img_2" src={banner_1} alt="" />
+                <img className="person" src={person} alt="" />
             </div>
         </>
     )
