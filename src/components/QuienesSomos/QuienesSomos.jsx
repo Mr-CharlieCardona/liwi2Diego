f96c6abd38fdc4d6a102d7454ab999297046be61
DiegoFMendezV <difemeva@hotmail.com>
2024-02-23 21:40:19 -0500

    ajustes css general

--- src/components/QuienesSomos/QuienesSomos.jsx
+++ src/components/QuienesSomos/QuienesSomos.jsx
@@ -3,18 +3,19 @@ import './QuienesSomos.css'
 function QuienesSomos() {
     return (
         <>
-        <hr className='container'/>
             <section id='QuienesSomos'>
-                <div className='container'>
-                    <h2>¿ QUIENES SOMOS ?</h2>
+            <img className="img1" src="https://img.freepik.com/vector-gratis/fondo-banner-tecnologia-formas-hexagonales-espacio-texto_1017-22589.jpg" alt="" />
+                <div className='context'>
+                    <h3>¿ QUIENES SOMOS ?</h3>
                     <p><b>InnerSoft</b> es una empresa de profesionales en informática con más de 15 años de experiencia en proporcionar soluciones de la más alta calidad en sistemas de información y automatización de procesos.</p>
-                    <h2>NUESTRA MISIÓN</h2>
+                    <h3>NUESTRA MISIÓN</h3>
                     <p>Es lograr que nuestros clientes obtengan los mejores beneficios de la tecnología de informática, mediante el suministro de soluciones integrales y de servicios de excelente calidad; además apoyar a nuestros colaboradores en busca de la excelencia y sus logros personales con la mentalidad del servicio nuestra comunidad.</p>
-                    <h2>¿ A QUE NOS COMPROMETEMOS ?</h2>
+                    <h3>¿ A QUE NOS COMPROMETEMOS ?</h3>
                     <p>Es lograr que nuestros clientes obtengan los mejores beneficios de la tecnología de informática, mediante el suministro de soluciones integrales y de servicios de excelente calidad; además apoyar a nuestros colaboradores en busca de la excelencia y sus logros personales con la mentalidad del servicio nuestra comunidad.</p>
                 </div>
                 <div className='img_quienes'>
-                    <img src="https://miro.medium.com/v2/resize:fit:1024/1*vxjAHkrXbGG6gOiPZgjeZA.jpeg" alt="" />
+                    <img id="img_q" src="https://miro.medium.com/v2/resize:fit:1024/1*vxjAHkrXbGG6gOiPZgjeZA.jpeg" alt="" />
+                    <img id="img_q2" src="https://concepto.de/wp-content/uploads/2018/09/Software-Libre1-e1536932868972-800x400.jpg" alt="" />
                 </div>
             </section>
         </>
